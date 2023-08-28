25e3b78ae802b36ef4325c49af69b5ec39786eb3
microintel <135629446+microintel@users.noreply.github.com>
2023-08-28 21:45:52 +0530

    Update CCJS.js

--- CCJS.js
+++ CCJS.js
@@ -1,51 +1,50 @@
 alert("Please choose your key value between 1 and 86 for better robust security.\n Remember your keys! 🛡️🔐 \nIf you forget your key, the output will be different. 🙈🔓!");
-        function ed() {
-      
-            function en(input, key) {
-                let enc = "";
-                let xc = "";
-                let xz = "";
-                for (let x of input) {
-                    enc += b[a.indexOf(x)]; }
-                xc += b.substr(key, 8);
-                xz += a.substr(key, 8);
-                let en = xc.substr(0, 4) + xz.substr(0, 4) + enc + xc.substr(4) + xz.substr(4);
-                document.getElementById("out").innerHTML = en;
-            }
+function ed() {
+    const a = " " + Array.from({ length: 94 }, (_, i) => String.fromCharCode(i + 33)).join("");
+    const input = document.getElementById("inp").value;
+    const key = parseInt(document.getElementById("key").value);
+    const b = a.substr(key) + a.substr(0, key);
+    const sele = document.getElementById("typ").value;
 
-            function de(input) {
-                let dec = "";
-                let xf = input.slice(8, -8);
-                for (let x of xf) {
-                    dec += a[b.indexOf(x)];
-                }
-                document.getElementById("out").innerHTML = dec;
-            }
+    if (key < 1 || key > 86) {
+        document.getElementById("out").textContent = "Invalid key value. Please choose a key between 1 and 86.";
+        return;
+    }
 
-            let a = "";
-            for (let c = 33; c < 127; c++) {
-                a += String.fromCharCode(c);
-            }
-            
-            a += " ";
+    if (sele === "e") {
+        en(input, key, a, b);
+    } else if (sele === "d") {
+        de(input, a, b);
+    } else {
+        document.getElementById("out").textContent = "Invalid operation";
+    }
+}
 
-            let input = document.getElementById("inp").value;
-            let key = parseInt(document.getElementById("key").value);
-            let b = a.substr(key) + a.substr(0, key);
-            let sele = document.getElementById("typ").value;
+function en(input, key, a, b) {
+    let enc = "";
+    for (let x of input) {
+        enc += b[a.indexOf(x)];
+    }
+    let xc = b.substr(key, 8);
+    let xz = a.substr(key, 8);
+    let en = xc.substr(0, 4) + xz.substr(0, 4) + enc + xc.substr(4) + xz.substr(4);
+    document.getElementById("out").innerHTML = en;
+}
 
-            if (sele === "e") {
-                en(input, key);
-            } else if (sele === "d") {
-                de(input);
-            } else {
-                document.getElementById("out").textContent = "Invalid operation";
-            }
-        }
-        function ref(){
-        location.reload();
-        }
-    function cpy() {
+function de(input, a, b) {
+    let dec = "";
+    let xf = input.slice(8, -8);
+    for (let x of xf) {
+        dec += a[b.indexOf(x)];
+    }
+    document.getElementById("out").innerHTML = dec;
+}
+
+function ref() {
+    location.reload();
+}
+
+function cpy() {
     const x = document.getElementById("out").textContent;
     
     const x1 = document.createElement("textarea");
@@ -58,5 +57,4 @@ alert("Please choose your key value between 1 and 86 for better robust security.
     document.body.removeChild(x1);
     
     alert("Copied to clipboard");
-    }
-    
+}
